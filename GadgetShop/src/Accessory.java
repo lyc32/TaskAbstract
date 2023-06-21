0937449b2533d3324aab107da7319cfbb6851bae
lyc32 <liyc930826@gmail.com>
2023-06-21 12:02:54 -0500

    GadgetShop

---
+++ GadgetShop/src/Accessory.java
@@ -0,0 +1,34 @@
+public class Accessory extends Categorize
+{
+    private Item[] itemList;
+
+    public Accessory()
+    {
+        super("Accessories");
+    }
+
+    @Override
+    public void setItemList(Item[] itemList)
+    {
+        this.itemList = itemList;
+    }
+
+    @Override
+    public Item[] getItemList()
+    {
+        return this.itemList;
+    }
+
+    @Override
+    public void setSubCategorize(Categorize[] subCategorizeList)
+    {
+
+    }
+
+    @Override
+    public Categorize[] getSubCategorize()
+    {
+        return null;
+    }
+
+}
---
+++ GadgetShop/src/LaptopMac.java
@@ -0,0 +1,31 @@
+public class LaptopMac extends Categorize
+{
+    private Item[] itemList;
+
+    public LaptopMac()
+    {
+        super("Mac");
+    }
+
+    @Override
+    public void setItemList(Item[] itemList)
+    {
+        this.itemList = itemList;
+    }
+
+    @Override
+    public Item[] getItemList()
+    {
+        return this.itemList;
+    }
+
+    @Override
+    public void setSubCategorize(Categorize[] subCategorizeList) { }
+
+    @Override
+    public Categorize[] getSubCategorize()
+    {
+        return null;
+    }
+
+}
